6e37052f901f728a55898fd465bb903b2757e135
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-11-23 09:41:13 -0300

    finish json

---
+++ tipos/tipoInterface/index.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var coisa interface{}
+	fmt.Println(coisa)
+	coisa = 3
+	fmt.Println(coisa)
+
+	type dinamico interface{}
+
+	var coisa2 dinamico = "Opa"
+	fmt.Println(coisa2)
+
+	//coisa2 = curso{"Go Lang"}
+	fmt.Println(coisa2)
+}
