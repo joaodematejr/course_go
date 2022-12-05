d84b7ec615290e03b1215e141dbf038905342acd
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-12-05 11:52:44 -0300

    reuso

---
+++ pacote/reuso/index.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/joaodematejr/area"
+)
+
+func main() {
+	fmt.Println(area.Circle(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+	//fmt.Println(area._TrianguloEq(5.0, 2.0))
+}
