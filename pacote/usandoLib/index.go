91f9a2127cacd9323aaa5eecd9713323aba9c78d
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-12-05 13:23:38 -0300

    pacotes

---
+++ pacote/usandoLib/index.go
@@ -0,0 +1,12 @@
+package main
+
+//usando go install github.com/cod3rcursos/goarea@latest
+import (
+	"fmt"
+
+	"github.com/cod3rcursos/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
