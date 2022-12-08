514de5ed62ce892d1b62acfe4c518e1170c72946
joaodematejr <joao.demate@alquimidiacorp.com.br>
2022-12-08 11:16:58 -0300

    generator

---
+++ concorrencia/generator/index.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Google I/O 2012 - Go Concurrency Patterns
+
+// https://www.youtube.com/watch?v=f6kdp27TYZs
+
+// <-chan - canal somente-leitura
+func Titulo(urls ...string) <-chan string {
+	c := make(chan string)
+
+	for _, url := range urls {
+		go func(cUrl string) {
+			resp, _ := http.Get(cUrl)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			aRetorno := r.FindStringSubmatch(string(html))
+
+			// teste para evitar erro
+			if cap(aRetorno) == 0 {
+				c <- "Erro ao ler página " + cUrl
+				return
+			}
+
+			c <- aRetorno[1]
+		}(url)
+	}
+
+	return c
+}
+
+func main() {
+	t1 := Titulo("https://www.cod3r.com.br", "https://www.google.com")
+	t2 := Titulo("https://www.amazon.com", "https://www.youtube.com")
+	fmt.Println("Primeiros:", <-t1, "|", <-t2)
+	fmt.Println("Segundos:", <-t1, "|", <-t2)
+}
